1f6e0a7947b62cd88a8d909362d3a86936c25696
Peter Fitzgerald <143887269+Atychi762@users.noreply.github.com>
2026-01-16 11:31:11 +0000

    Changing the format of the fitness function file

---
+++ fitnessEvaluator.py
@@ -0,0 +1,12 @@
+def evaluateFitness(obj):
+    # Evaluate the fitness of a given object
+    fitness_score = 0
+
+    for i in range(len(obj)):
+        fitness_score += obj[i]
+
+    return fitness_score / len(obj)
+
+
+obj = [1, 1, 0, 0, 0]
+print(evaluateFitness(obj))
