b37789f409c59616655fe18661d26f12ea0becdb
Peter Fitzgerald <143887269+Atychi762@users.noreply.github.com>
2026-01-16 12:02:02 +0000

    Adding basic mutator method

---
+++ mutator.py
@@ -0,0 +1,11 @@
+import random
+
+def mutate(population, mutation_rate):
+    for individual in population:
+        if random.random() < mutation_rate:
+            for value in range(len(individual)):
+                if random.random() < len(individual) / 100:
+                    individual[value] = 1 - individual[value]
+                    print("Bit mutated")
+
+    return population
