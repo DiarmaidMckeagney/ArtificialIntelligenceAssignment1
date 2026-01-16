2d72c6fa468c69e50f8c0a10a2fb8908ed93988c
DiarmaidMckeagney <117989065+DiarmaidMckeagney@users.noreply.github.com>
2026-01-16 14:01:55 +0000

    added hyperparameter tuning

--- mutator.py
+++ mutator.py
@@ -6,5 +6,4 @@ def mutate(population, mutation_rate):
             for value in range(len(individual)):
                 if random.random() < len(individual) / 100:
                     individual[value] = 1 - individual[value]
-    print("mutated")
     return population
